7c50e9d17bd5d0ebefb1b83979e4c9d13b45f41a
andrew54068 <andrew0424718012@yahoo.com.tw>
2019-10-13 01:08:15 +0800

    [initial commit] support from iOS 11.

---
+++ IOS@Taipei/IOS@Taipei/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  IOS@Taipei
+//
+//  Created by kidnapper on 2019/10/13.
+//  Copyright © 2019 iOS@Taipei. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IOS@Taipei/IOS@Taipei/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  IOS@Taipei
+//
+//  Created by kidnapper on 2019/10/13.
+//  Copyright © 2019 iOS@Taipei. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
