68137b3b784b39b5abf1414a4a45f3c2bf557ae6
aaron_ni <aaron_ni@Aaron-Nide-MacBook-Pro.local>
2019-10-20 12:39:55 +0800

    VCs

--- IOS@Taipei/IOS@Taipei/UIs/VCs/Tab/Event/EventDetailVC.swift
+++ IOS@Taipei/IOS@Taipei/UIs/VCs/Tab/Event/EventDetailVC.swift
@@ -1,20 +1,19 @@
 //
-//  SecondViewController.swift
+//  EventVC.swift
 //  IOS@Taipei
 //
-//  Created by kidnapper on 2019/10/13.
+//  Created by Aaron_Ni on 2019/10/20.
 //  Copyright © 2019 iOS@Taipei. All rights reserved.
 //
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class EventDetailVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
