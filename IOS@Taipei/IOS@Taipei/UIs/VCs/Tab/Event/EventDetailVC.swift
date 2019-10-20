8ea6fe50ae5f4d5a49a5eb55748cda7e2ba1ee2e
aaron_ni <aaron_ni@Aaron-Nide-MacBook-Pro.local>
2019-10-20 16:23:29 +0800

    Setup EventDetail's label.

--- IOS@Taipei/IOS@Taipei/UIs/VCs/Tab/Event/EventDetailVC.swift
+++ IOS@Taipei/IOS@Taipei/UIs/VCs/Tab/Event/EventDetailVC.swift
@@ -9,11 +9,43 @@
 import UIKit
 
 class EventDetailVC: UIViewController {
+    
+    @IBOutlet private weak var titleImageView: UIImageView!
+    @IBOutlet private weak var timeLabel: UILabel!
+    @IBOutlet private weak var topicLabel: UILabel!
+    
+    @IBOutlet private weak var speakerNameLabel: UILabel!
+    @IBOutlet private weak var applicantsAmountLabel: UILabel!
+    @IBOutlet private weak var addressLabel: UILabel!
+    
+    @IBOutlet private weak var descTitleLabel: UILabel!
+    @IBOutlet private weak var descContentLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        timeLabel
+            .set(\.font, to: .app_eventDetailTime)
+            .set(\.textColor, to: .app_eventDetailTime)
+        topicLabel
+            .set(\.font, to: .app_eventDetailTopic)
+            .set(\.textColor, to: .app_text)
+        
+        speakerNameLabel
+            .set(\.font, to: .app_eventDetailInfo)
+            .set(\.textColor, to: .app_text)
+        applicantsAmountLabel
+            .set(\.font, to: .app_eventDetailInfo)
+            .set(\.textColor, to: .app_text)
+        addressLabel
+            .set(\.font, to: .app_eventDetailInfo)
+            .set(\.textColor, to: .app_text)
+        
+        descTitleLabel
+            .set(\.font, to: .app_eventDetailDescTitle)
+            .set(\.textColor, to: .app_text)
+        descContentLabel
+            .set(\.font, to: .app_eventDetailDescContent)
+            .set(\.textColor, to: .app_text)
     }
-    
 }
